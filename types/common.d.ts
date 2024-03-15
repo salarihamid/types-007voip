7aaa674eb649b7906b3c7901310f2c27c4694dad
Hamid Salari <us13@yahoo.com>
2024-03-15 15:18:33 +0330

    More Types

--- types/common.d.ts
+++ types/common.d.ts
@@ -2,6 +2,11 @@ export namespace Common {
 
 }
 
+export enum SYSTEM_ROLES {
+    admin = "admin",
+    user = "user"
+}
+
 export enum NODE_ENV {
     production = "production",
     development = "development",
